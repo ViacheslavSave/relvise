f1a46b6d9885af947165abbebc0c3b1c96313a56
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-04-03 18:15:35 +0300

    верстка закончена

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ function count(element, colums) {
 		}
 	};
 }
+
+// let getintouch__items = document.querySelector(".getintouch__items");
+let itemGetintouch = document.querySelectorAll(".item-getintouch");
+
+itemGetintouch.forEach((element) => {
+	element.addEventListener("mouseover", count2(element, itemGetintouch));
+});
+// getintouch__items.addEventListener("mouseover", );
+
+function count2(element, colums) {
+	return (e) => {
+		if (e.target.closest(".item-getintouch")) {
+			colums.forEach((item) => {
+				item.classList.remove("item-getintouch__active");
+				let icon = item.querySelector(".item-getintouch__icon > img");
+				icon.setAttribute("src", icon.getAttribute("src").replace(/-active/g, ""));
+			});
+			// console.log(e);
+			element.classList.add("item-getintouch__active");
+			icon = element.querySelector(".item-getintouch__icon > img");
+			icon.setAttribute("src", icon.getAttribute("src").replace(/.svg/g, "") + "-active.svg");
+			// console.log(icon.getAttribute("src"));
+		}
+	};
+}
